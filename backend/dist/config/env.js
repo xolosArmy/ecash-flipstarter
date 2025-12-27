2a4b54de45dff033586d6c6b34e7d7cc485818a0
xolosarmy <contacto@xolosramirez.com>
2025-12-27 11:03:46 -0600

    chore: stop tracking node_modules, dist, and local env

--- backend/dist/config/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = void 0;
-exports.env = {
-    rpcUrl: process.env.ECASH_RPC_URL || process.env.E_CASH_RPC_URL || 'http://localhost:8332',
-    network: process.env.ECASH_NETWORK || 'testnet',
-};
--- backend/dist/covenants/scriptCompiler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.compileCampaignScript = compileCampaignScript;
-function compileCampaignScript(_campaign) {
-    // TODO: integrate actual covenant compilation and hashing for eCash scripts.
-    return { scriptHex: '51', scriptHash: 'hash-placeholder' };
-}
